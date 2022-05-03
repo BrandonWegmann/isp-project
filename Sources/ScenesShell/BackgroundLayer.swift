c283ee3089352bee6440257287bc42cf3bf8cc27
Brandon Wegmann <brandon.wegmann@student.allenisd.org>
2022-05-02 22:26:27 -0500

    Basic Changes

--- Sources/ScenesShell/BackgroundLayer.swift
+++ Sources/ScenesShell/BackgroundLayer.swift
@@ -7,13 +7,18 @@ import Scenes
 
 
 class BackgroundLayer : Layer {
-      let background = Background()
+    let background = Background()
+    let gameover = GameOver()
+    static let leftScoreboard = Scoreboard()
+    static let rightScoreboard = Scoreboard()
+    static let topScoreboard = Scoreboard()
+    static let bottomScoreboard = Scoreboard()
 
-      init() {
-          // Using a meaningful name can be helpful for debugging
-          super.init(name:"Background")
+    init() {
+        // Using a meaningful name can be helpful for debugging
+        super.init(name:"Background")
 
-          // We insert our RenderableEntities in the constructor
-          insert(entity:background, at:.back)
-      }
-  }
+        // We insert our RenderableEntities in the constructor
+            insert(entity:background, at:.back)
+    }
+}
